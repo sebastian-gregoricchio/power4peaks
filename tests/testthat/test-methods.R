52149afc96635293279c139af57d1c7662344e39
Sebastian Gregoricchio <sebastian.gregoricchio@gmail.com>
2025-09-10 19:01:43 +0200

    Update test-methods.R

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -1,4 +1,4 @@
 test_that("power4peaks.stats show-method", {
   p4p = as.power4peaks(dba.object = power4peaks::endometrium)
-  expect_no_failure(plot.stat.distribution(power4peaks.stats = p4p))
+  expect_no_failure(show(p4p))
 })
